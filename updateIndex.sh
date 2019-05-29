930161656fed34f65083692a92ec1d9761317273
Dennis Ploeger <dennis.ploeger@kps.com>
2019-05-29 16:06:07 +0200

    chore: Updated documentation index

--- updateIndex.sh
+++ updateIndex.sh
@@ -3,6 +3,6 @@
 rm documentationIndex.json
 rm indexErrors.txt
 git submodule update --remote --merge
-TFLOOKUP_INDEXFILE=documentationIndex.json TFLOOKUP_STORE_INDEX=true TFLOOKUP_START_SERVER=false node index.js 2>&1 | grep "Can't load" | awk '{ print $7 }' | sed -re "s/:$//gi" > indexErrors.txt
+TFLOOKUP_INDEXFILE=documentationIndex.json TFLOOKUP_STORE_INDEX=true TFLOOKUP_START_SERVER=false node index.js 2>&1 | grep "Can't load" | awk '{ print $7 }' | sed -e "s/:$//g" > indexErrors.txt
 git commit -am "chore: Updated documentation index"
 git push origin master
